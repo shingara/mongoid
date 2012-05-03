71fcfa5a2246137fe32f80660075f800eb17b71a
Cyril Mougel <cyril.mougel@gmail.com>
2012-05-03 16:05:28 +0200

    add spec about issue #1973

---
+++ spec/issues/issue_1973_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe "Issue 1973" do
+
+  class Match
+    include Mongoid::Document
+    include Mongoid::Timestamps
+
+    belongs_to :user1, :class_name => "User", :inverse_of => 'hosted_matches'
+    belongs_to :user2, :class_name => "User", :inverse_of => 'invited_matches'
+  end
+
+  class User
+    include Mongoid::Document
+    include Mongoid::Timestamps
+
+    has_many :hosted_matches, :class_name => "Match", :foreign_key => "user1_id"
+    has_many :invited_matches, :class_name => "Match", :foreign_key => "user2_id"
+  end
+
+  it 'should not link same user' do
+    @user1 = User.create
+    @user2 = User.create
+    @match = Match.new
+
+    @match.user1 = @user1
+    @match.user2 = @user2
+    @match.save!
+    @match.reload
+    @match.user1.should_not eq(@match.user2)
+  end
+
+end
